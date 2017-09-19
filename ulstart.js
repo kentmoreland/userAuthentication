f8c6c805b9b5d3b7bd28b8b97a465de215076a91
Kent <kentmoreland@gmail.com>
2017-09-18 20:14:07 -0500

    update: added mongoose configuration

--- ulstart.js
+++ ulstart.js
@@ -1,4 +1,15 @@
+const mongoose = require('mongoose');
 const ul = require('./ul');
+
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+  console.log('error');
+});
+
+mongoose.connection.on('open', () => {
+  console.log('database is runnin!');
+});
 
 
 ul.listen(ul.get('port'), () => {
