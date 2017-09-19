1a12df90b1232756882ad2124b46e456da6285e2
Kent <kentmoreland@gmail.com>
2017-09-19 07:36:23 -0500

    update: added bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = mongoose.Schema({
   local: {
@@ -6,5 +7,13 @@ const userSchema = mongoose.Schema({
     password: String,
   },
 });
+
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
